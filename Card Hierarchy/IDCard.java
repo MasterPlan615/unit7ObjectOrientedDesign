4d8226161d0d267b123139004f6babb878d82978
Andrew Souhrada <andrewsouhrada@yahoo.com>
2016-02-03 14:14:00 -0600

    Update

--- Card Hierarchy/IDCard.java
+++ Card Hierarchy/IDCard.java
@@ -17,4 +17,22 @@ public class IDCard extends Card
    {
        return super.format() + ", ID Number: " + this.idNumber;
    }
+   
+   // override tge equals method defined in Card as well as Object
+   public boolean equals( Object other )
+   {
+       // check if this object and the other object are both of the same class
+       if( this.getClass() == other.getClass() )
+       {
+           // first check if this object and the other object are equal from the perspective of the superclass (i.e., Card)
+           boolean isEqual = super.equals( otherCard );
+           
+           // second, cast the other object to an IDCard and check if idNumber are equal
+           
+           IDCard otherIDCard = (IDCard) other;
+           return isEqual && this.idNumber.equals( otherCard.idNumber );
+       }
+       
+       return false;
+   }
 }
